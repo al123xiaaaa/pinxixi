760bf72e715bb01350a1a23cee804efcc4c38d7a
Guangwei Xia <al123xiaaaa@hotmail.com>
2020-07-14 22:12:12 +0800

    encapsulating scrollbar first step isolate to components folder

---
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+interface TopMenu {
+  title: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-scrollable-tab',
+  templateUrl: './scrollable-tab.component.html',
+  styleUrls: ['./scrollable-tab.component.sass']
+})
+export class ScrollableTabComponent implements OnInit {
+
+  constructor() { }
+
+  selectedIndex = -1;
+  menus: TopMenu[] = [
+    { title: '热门', link: '' },
+    { title: '男装', link: '' },
+    { title: '运动', link: '' },
+    { title: '手机', link: '' },
+    { title: '家纺', link: '' },
+    { title: '食品', link: '' },
+    { title: '电器', link: '' },
+    { title: '鞋包', link: '' },
+    { title: '汽车', link: '' },
+    { title: '水果', link: '' },
+    { title: '电脑', link: '' },
+    { title: '内衣', link: '' },
+    { title: '家装', link: '' },
+    { title: '母婴', link: '' },
+    { title: '美妆', link: '' },
+    { title: '家具', link: '' },
+    { title: '书籍', link: '' },
+  ];
+
+  handleSelection(index: number) {
+    this.selectedIndex = index;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
